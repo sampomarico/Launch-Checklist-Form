4c66fab3e2e81b233c3680a10f45370a918e4cca
Sam <samtpomarico@gmail.com>
2020-09-08 20:24:37 -0500

    Added fetching planetary json data

--- script.js
+++ script.js
@@ -58,8 +58,23 @@ window.addEventListener("load", function() {
          document.getElementById("launchStatus").style.color = "green";
          document.getElementById("launchStatus").innerHTML = `Shuttle is ready for launch`;
          event.preventDefault();
+         
       }
-
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+         response.json().then(function(json) {
+           const div = document.getElementById("missionTarget");
+           div.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[0].name}</li>
+               <li>Diameter: ${json[0].diameter}</li>
+               <li>Star: ${json[0].star}</li>
+               <li>Distance from Earth: ${json[0].distance}</li>
+               <li>Number of Moons: ${json[0].moons}</li>
+            </ol>
+            <img src="${json[0].image}">`
+         });
+      });
 
    });
 });
