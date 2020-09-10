359a419312691e99566977157467a994c8238f78
Sam <samtpomarico@gmail.com>
2020-09-09 21:44:53 -0500

    Added bonus mission. Assignment complete

--- script.js
+++ script.js
@@ -62,17 +62,18 @@ window.addEventListener("load", function() {
       }
       fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
          response.json().then(function(json) {
+           let randomNumber = Math.floor(Math.random()*5);
            const div = document.getElementById("missionTarget");
            div.innerHTML = `
             <h2>Mission Destination</h2>
             <ol>
-               <li>Name: ${json[0].name}</li>
-               <li>Diameter: ${json[0].diameter}</li>
-               <li>Star: ${json[0].star}</li>
-               <li>Distance from Earth: ${json[0].distance}</li>
-               <li>Number of Moons: ${json[0].moons}</li>
+               <li>Name: ${json[randomNumber].name}</li>
+               <li>Diameter: ${json[randomNumber].diameter}</li>
+               <li>Star: ${json[randomNumber].star}</li>
+               <li>Distance from Earth: ${json[randomNumber].distance}</li>
+               <li>Number of Moons: ${json[randomNumber].moons}</li>
             </ol>
-            <img src="${json[0].image}">`
+            <img src="${json[randomNumber].image}">`
          });
       });
 
